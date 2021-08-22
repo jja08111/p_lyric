9f3741ab653596f65e51f9b12388040da25e97f2
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2021-08-22 15:20:13 +0900

    Add git action (#24)
    * Add git action
    
    * Fix code formatting
    
    * Fix file
    
    * Remove test content

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:p_lyric/main.dart';
 
 void main() {
-  testWidgets('test appbar title', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-    expect(find.text('PLyric'), findsOneWidget);
+  testWidgets('Temp test', (WidgetTester tester) async {
+
   });
 }
