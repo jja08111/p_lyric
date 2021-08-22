5dac980cb8ecd03b64b004a1328c5f9d34e70d5e
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2021-08-22 12:41:26 +0900

    기기에서 재생중인 음악의 정보 및 가사를 보여주는 기능 (#10) (#11)
    * Import nowplaying plugin
    
    * Change nowplaying service label to PLyric in AndroidManifest.xml
    
    * Implement playing_music_provider.dart
    
    * Implement CardView in HomePage

---
+++ lib/provider/playing_music_provider.dart
@@ -0,0 +1,32 @@
+import 'package:get/get.dart';
+import 'package:nowplaying/nowplaying.dart';
+import 'package:p_lyric/servies/melon_lyric_scraper.dart';
+
+class PlayingMusicProvider extends GetxController {
+  PlayingMusicProvider();
+
+  Rx<NowPlayingTrack> _track = NowPlayingTrack.notPlaying.obs;
+  NowPlayingTrack? get track => _track.value;
+
+  String _lyric = '';
+  String get lyric => _lyric;
+
+  @override
+  void onInit() {
+    super.onInit();
+    NowPlaying.instance.stream.listen(_listenTrackEvent);
+    debounce(_track, _updateLyric, time: const Duration(milliseconds: 200));
+  }
+
+  void _updateLyric(NowPlayingTrack track) async {
+    _lyric = await MelonLyricScraper.searchLyric(track.title ?? '');
+    update();
+  }
+
+  void _listenTrackEvent(NowPlayingTrack newTrack) {
+    if (newTrack.isPlaying && _track.value.title != newTrack.title) {
+      _track.value = newTrack;
+      update();
+    }
+  }
+}
