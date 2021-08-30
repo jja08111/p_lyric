6ea3a7316e442082ae64600a2b5543e0cec8153f
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2021-08-31 00:47:29 +0900

    SystemAlertWindow 권한 및 초기 세팅

--- android/app/src/main/kotlin/com/example/p_lyric/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/p_lyric/MainActivity.kt
@@ -1,6 +1,18 @@
 package com.example.p_lyric
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import `in`.jvapps.system_alert_window.SystemAlertWindowPlugin
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterActivity(), PluginRegistrantCallback {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        SystemAlertWindowPlugin.setPluginRegistrant(this)
+    }
+
+    override fun registerWith(registry: PluginRegistry) {
+        SystemAlertWindowPlugin.registerWith(registry.registrarFor("in.jvapps.system_alert_window"));
+    }
 }
