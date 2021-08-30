c0b7b782ba53a78a882985bec20a8ce0caaeeeae
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2021-08-31 00:47:29 +0900

    SystemAlertWindow 권한 및 초기 세팅

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:nowplaying/nowplaying.dart';
 import 'package:p_lyric/provider/music_provider.dart';
+import 'package:p_lyric/provider/permission_provider.dart';
 import 'package:p_lyric/views/setting_page.dart';
-import 'package:p_lyric/widgets/default_bottom_sheet.dart';
 import 'package:p_lyric/widgets/default_container.dart';
-import 'package:p_lyric/widgets/default_snack_bar.dart';
 import 'package:p_lyric/widgets/subtitle.dart';
 
 class HomePage extends StatefulWidget {
@@ -28,14 +27,7 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     _scrollController.addListener(_updateScrollButton);
 
-    NowPlaying.instance.isEnabled().then((bool isEnabled) async {
-      if (!isEnabled) {
-        await Get.bottomSheet(
-          const _PermissionBottomSheet(),
-          isDismissible: false,
-        );
-      }
-    });
+    Get.put(PermissionProvider());
   }
 
   @override
@@ -171,108 +163,6 @@ class _HomePageState extends State<HomePage> {
           duration: kThemeChangeDuration,
           child: showButton ? child! : const SizedBox(),
         ),
-      ),
-    );
-  }
-}
-
-class _PermissionBottomSheet extends StatefulWidget {
-  const _PermissionBottomSheet({Key? key}) : super(key: key);
-
-  @override
-  _PermissionBottomSheetState createState() => _PermissionBottomSheetState();
-}
-
-class _PermissionBottomSheetState extends State<_PermissionBottomSheet>
-    with WidgetsBindingObserver {
-  bool _inProgress = false;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance!.addObserver(this);
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance!.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) async {
-    switch (state) {
-      case AppLifecycleState.resumed:
-        if (_inProgress) {
-          if (await NowPlaying.instance.isEnabled()) {
-            Get.back();
-            showSnackBar('권한 허용됨');
-          }
-          _inProgress = false;
-        }
-        break;
-      default:
-    }
-  }
-
-  void _onPressedSkip() async {
-    Get.back();
-    showSnackBar('설정에서 권한을 설정할 수 있습니다.');
-  }
-
-  void _onPressedOk() async {
-    _inProgress = true;
-    await NowPlaying.instance.requestPermissions(force: true);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final textTheme = Get.textTheme;
-    final colorScheme = Get.theme.colorScheme;
-
-    return DefaultBottomSheet(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            'PLyric 앱의 알림 접근 권한을 허용해주세요.',
-            style: textTheme.headline5!.copyWith(height: 1.4),
-          ),
-          const SizedBox(height: 8.0),
-          Text(
-            '현재 재생중인 음악 정보를 얻기 위해 필요합니다.',
-            style: textTheme.bodyText2!.copyWith(
-              color: Color(0xb3000000),
-              height: 1.6,
-            ),
-          ),
-          const SizedBox(height: 16.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              TextButton(
-                onPressed: _onPressedSkip,
-                child: Text(
-                  '건너뛰기',
-                  style: textTheme.button!.copyWith(
-                    color: colorScheme.secondaryVariant,
-                  ),
-                ),
-              ),
-              const SizedBox(width: 8.0),
-              TextButton(
-                onPressed: _onPressedOk,
-                child: Text(
-                  '설정하기',
-                  style: textTheme.button!.copyWith(
-                    color: colorScheme.primaryVariant,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
       ),
     );
   }
