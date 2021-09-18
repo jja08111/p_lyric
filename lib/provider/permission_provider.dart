86d3a679e4f242391d788415ceef88c05d093c1f
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2021-09-18 12:30:00 +0900

    플로팅 오버레이 윈도우 구현 (#66)
    * 오버레이를 위한 권한 추가 및 기반 작업 (#56)
    
    * SystemAlertWindow 권한 및 초기 세팅
    
    * SystemAlertWindow 권한 및 초기 세팅
    
    * SystemAlertWindow 권한 및 초기 세팅
    
    * 권한 허용 후 BottomSheet가 내려가지 않는 문제 수정
    
    * 코드 간결화
    
    * 플로팅 창 전환 아이콘 홈 페이지에 추가
    
    * SystemAlertWindow 플러그인 삭제
    
    * SystemAlertWindow 플러그인 삭제
    
    * Gradle 버전 4.2.2로 다운그레이드
    
    아래에서 관련 이슈 내용 확인 가능
    https://github.com/flutter/flutter/issues/87649
    
    * 플로팅 뷰 프로토타입 구현
    
    * Fix permission in AndroidManifest.xml
    
    * Update nowplaying plugin
    
    * 사용자가 명시적으로 권한을 허용 or 건너뛰기를 한 경우만 저장
    
    * HomePage의 축소 버튼 기능 구현
    
    * 포어그라운드 작동 업데이트
    
    * 홈 페이지 축소 버튼 버그 해결 및 플로팅 뷰 안정성 향상
    
    * 플로팅 윈도우 백그라운드 작동 및 앨범 커버 적용 등 업데이트 (#65)
    
    * SystemAlertWindow 권한 및 초기 세팅
    
    * SystemAlertWindow 권한 및 초기 세팅
    
    * SystemAlertWindow 권한 및 초기 세팅
    
    * 권한 허용 후 BottomSheet가 내려가지 않는 문제 수정
    
    * 코드 간결화
    
    * 플로팅 창 전환 아이콘 홈 페이지에 추가
    
    * SystemAlertWindow 플러그인 삭제
    
    * SystemAlertWindow 플러그인 삭제
    
    * Gradle 버전 4.2.2로 다운그레이드
    
    아래에서 관련 이슈 내용 확인 가능
    https://github.com/flutter/flutter/issues/87649
    
    * 플로팅 뷰 프로토타입 구현
    
    * Fix permission in AndroidManifest.xml
    
    * Update nowplaying plugin
    
    * 사용자가 명시적으로 권한을 허용 or 건너뛰기를 한 경우만 저장
    
    * HomePage의 축소 버튼 기능 구현
    
    * 포어그라운드 작동 업데이트
    
    * 홈 페이지 축소 버튼 버그 해결 및 플로팅 뷰 안정성 향상
    
    * Update to show album cover and tap gesture handling
    
    - https://github.com/jja08111/nowplaying/commit/a115a9376cc01b598144774d83291d2182612f25
    
    * 백그라운드에서 작동하도록 수정
    
    - 사용자가 명시적으로 홈 페이지의 축소 버튼을 눌러야만 플로팅 윈도우 서비스 실행
    - 앱을 종료해도 음악 업데이트 시 플로팅 뷰를 위한 가사 검색이 작동할 수 있도록 수정
    
    * 플러그인 연결 에러 수정 및 Merge로 인한 잘못된 코드 수정
    
    - https://github.com/jja08111/nowplaying/commit/cace5f57ab279ea11381cf14c038bda80ef91b4f
    
    * 명시적으로 플로팅 뷰를 시작할때만 뷰를 띄우도록 수정
    
    - https://github.com/jja08111/nowplaying/commit/b4666de2fa2ba035240566e39dc356dd2a94481f
    
    * 다크모드 구현 및 초기 진입 에러 예외처리 구현
    
    - https://github.com/jja08111/nowplaying/commit/2cec21a511f71cd9bd8071c3aed35ca495a1da26

---
+++ lib/provider/permission_provider.dart
@@ -0,0 +1,50 @@
+import 'package:get/get.dart';
+import 'package:nowplaying/nowplaying.dart';
+import 'package:p_lyric/widgets/permission_bottom_sheet.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class PermissionProvider {
+  static const String alreadyRequestedKey = 'permission.alreadyRequested';
+
+  PermissionProvider() {
+    _initialize();
+  }
+
+  void _initialize() async {
+    final prefs = await SharedPreferences.getInstance();
+    final alreadyRequested = prefs.getBool(alreadyRequestedKey) ?? false;
+    final hasSystemOverlay = await hasSystemOverlayPermission();
+    final hasNotificationAccess = await hasNotificationAccessPermission();
+
+    if (!alreadyRequested && (!hasSystemOverlay || !hasNotificationAccess)) {
+      await Get.bottomSheet(
+        const PermissionBottomSheet(),
+        isDismissible: false,
+      );
+    }
+  }
+
+  Future<bool> haveAllPermissions() async {
+    return (await hasSystemOverlayPermission()) &&
+        (await hasNotificationAccessPermission());
+  }
+
+  Future<bool> hasSystemOverlayPermission() async {
+    return Permission.systemAlertWindow.isGranted;
+  }
+
+  Future<bool> hasNotificationAccessPermission() async {
+    return NowPlaying.instance.isEnabled();
+  }
+
+  Future<PermissionStatus> requestSystemOverlayPermission() async {
+    return Permission.systemAlertWindow.request();
+  }
+
+  // nowPlaying 플러그인이 권한 요구 창을 성공적으로 띄웠을 때 바로 값을 반환하기 때문에 반환값이
+  // `Future<void>`가 아닌 `void`이다.
+  void requestNotificationAccessPermission() async {
+    NowPlaying.instance.requestPermissions(force: true);
+  }
+}
