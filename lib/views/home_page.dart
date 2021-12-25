1e92ce328e3b36caeeb65739c5e427299cbe139f
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2021-12-25 09:16:36 +0900

    Upgrade flutter, dart version and fix NameNotFoundException warning (#73)
    * Bump flutter and dart version
    
    * Fix NameNotFoundException by adding permission in AndroidManifest.xml

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -461,7 +461,7 @@ class _AlbumCoverImage extends StatelessWidget {
               duration: kThemeChangeDuration,
               child: hasImage
                   ? Image(
-                      image: image!,
+                      image: image,
                       height: imageDiameter,
                       width: imageDiameter,
                     )
