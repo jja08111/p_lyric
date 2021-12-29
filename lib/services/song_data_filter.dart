8288613535541d6077e8ed8c374f5b927497bd2e
Sihyun Jung <54366260+alpha-src@users.noreply.github.com>
2021-12-29 14:59:16 +0900

    노래 데이터 전처리 클래스 적용 및 단순화 (#81)
    * 검색 쿼리를 Uri encode 하는 형식으로 변경
    
    * Handle special characters for search query
    
    * Add necessary files for #72
    
    * Apply suggestions from #72
    
    * Apply suggestions from #72 - Move Exception class in bugs_lyrics_scraper.dart
    
    * Apply suggestion from #72 - Move exception classes in bugs_lyrics_scraper.dart
    
    * solve getLyricsFromBugs data type problem
    
    * Delete fromGenius & Delete Exception class & Change variable name
    
    * 안드로이드 출시를 위한 난독화, 암호화와 앱 서명
    
    * README 파일 업데이트
    
    * 패키지 이름 변경
    
    * Apply Song data preprocessor method to music provider
    
    * Revert "Apply Song data preprocessor method to music provider"
    
    This reverts commit 6a3e924b7d9ef491dd54d3472e2fd1e9406252d4.
    
    * 전처리 과정 `getLyricsFromBugs()` 함수에 추가
    
    * extension을 이용하여 SongDataFilter로 수정
    
    Co-authored-by: Cirrus CI <support@cirruslabs.org>
    Co-authored-by: jungsiroo <siroo@jungsirooui-Macmini.local>
    Co-authored-by: Minseong Kim <57604817+jja08111@users.noreply.github.com>

--- lib/services/song_data_filter.dart
+++ lib/services/song_data_filter.dart
@@ -1,9 +1,9 @@
-class SongDataPreprocessor {
-  static RegExp _korean = RegExp(r"^[가-힣 ]*$");
-  static RegExp _english = RegExp(r"^[A-Za-zÀ-ȕ ]*$");
-  static RegExp _special = RegExp(r"^[0-9,_.\'\+%!@#$&?\-\ ]*$");
+extension SongDataFilter on String {
+  static final RegExp _korean = RegExp(r"^[가-힣 ]*$");
+  static final RegExp _english = RegExp(r"^[A-Za-zÀ-ȕ ]*$");
+  static final RegExp _special = RegExp(r"^[0-9,_.\'\+%!@#$&?\-\ ]*$");
 
-  static bool _isStartWithKorean(String title) {
+  bool _isStartWithKorean(String title) {
     bool ret;
     ret = _korean.hasMatch(title.split("")[0]) ? true : false;
 
@@ -11,7 +11,7 @@ class SongDataPreprocessor {
   }
 
   /// 영어와 한국어가 섞여 있을 경우, 첫 단어를 기준으로 그에 맞는 언어로만 구성된 `String` 을 리턴
-  static String _divideLanguage(String target) {
+  String _divideLanguage(String target) {
     final bool isKorean = _isStartWithKorean(target);
     String korExtract = "";
     String engExtract = "";
@@ -38,16 +38,16 @@ class SongDataPreprocessor {
   }
 
   /// 노래 제목을 벅스 검색에 최적화된 `filteredTitle` 을 반환한다.
-  static String filterSongTitle(String title) {
+  String filterSongTitle() {
     String filteredTitle = "";
 
-    filteredTitle = title.split("(피처링")[0];
+    filteredTitle = this.split("(피처링")[0];
     filteredTitle = filteredTitle.split("(")[0];
     filteredTitle = filteredTitle.split("[")[0];
 
-// 제목에 영어와 한국어 모두 포함되어있을 때
+    // 제목에 영어와 한국어 모두 포함되어있을 때
     if (!_korean.hasMatch(filteredTitle) && !_english.hasMatch(filteredTitle))
-      filteredTitle = _divideLanguage(title);
+      filteredTitle = _divideLanguage(filteredTitle);
 
     return filteredTitle.trim();
   }
@@ -58,10 +58,10 @@ class SongDataPreprocessor {
   /// 이때 가수명에 콤마와 `및` 이 포함된 경우는 `split`을 통해 앞에 있는 정보만 가져온다.
   /// 또한 괄호안에 가수의 영문을 써놓는 경우 제외시킨다.
   /// 위의 과정을 거쳐도 한글과 영문이 혼용되있을 경우도 마지막으로 필터링한다.
-  static String filterArtist(String artist) {
+  String filterSongArtist() {
     String filteredArtist = "";
 
-    filteredArtist = artist.split(", ")[0];
+    filteredArtist = this.split(", ")[0];
     filteredArtist = filteredArtist.split(" 및")[0];
     filteredArtist = filteredArtist.split("(")[0];
 
